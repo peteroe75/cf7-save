b596d63f4f71540c1a517f5649a69761a92c5b59
Peter Roe <89943260+peteroe75@users.noreply.github.com>
2026-02-25 10:42:13 -0500

    Update permissions.php
    author can view

--- includes/permissions.php
+++ includes/permissions.php
@@ -2,8 +2,15 @@
 if (!defined('ABSPATH')) exit;
 
 function ml_cf7_add_capability() {
-    $role = get_role('administrator');
-    if ($role) {
-        $role->add_cap('view_cf7_entries');
+
+    $roles = ['administrator', 'author'];
+
+    foreach ($roles as $role_name) {
+        $role = get_role($role_name);
+        if ($role) {
+            $role->add_cap('view_cf7_entries');
+        }
     }
 }
+
+add_action('init', 'ml_cf7_add_capability');
